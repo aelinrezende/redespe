e8a3d4394635d5efd3ee058c1c3a99d0e5b89029
Marco Rezende <marcorezende@outlook.com.br>
2020-09-20 23:47:48 -0300

    Updates

---
+++ precache-manifest.def2e7a1de6cf83d43d69956b95c912f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd799f9c02ced4c09ba6baa4b4e5aabb",
+    "url": "/redespe/index.html"
+  },
+  {
+    "revision": "ee5968d060a56e3a8b78",
+    "url": "/redespe/static/css/main.11c05b16.chunk.css"
+  },
+  {
+    "revision": "bb99d778503255c8a477",
+    "url": "/redespe/static/js/2.6f2f1a2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redespe/static/js/2.6f2f1a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5968d060a56e3a8b78",
+    "url": "/redespe/static/js/main.e759be7e.chunk.js"
+  },
+  {
+    "revision": "c56edf29f529635fd05f",
+    "url": "/redespe/static/js/runtime-main.2e392b61.js"
+  },
+  {
+    "revision": "9c1424d671459476e6227fe309d50e8a",
+    "url": "/redespe/static/media/arrow.9c1424d6.svg"
+  }
+]);
