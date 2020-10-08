142da6c561da2fac71df8ca9603dc4d0ef44c727
Marco Rezende <marcorezende@outlook.com.br>
2020-10-07 23:10:06 -0300

    bills list added

--- src/Pages/Expenses/index.tsx
+++ src/Pages/Expenses/index.tsx
@@ -1,46 +1,86 @@
 import React, { useState } from 'react';
+import AnimateHeight from 'react-animate-height';
 
 import './styles.scss';
 
 import Amount from '../../components/Amount/';
 
+import data from '../../data.json';
+
+import { ReactComponent as Arrow } from '../../assets/icons/arrow_down.svg';
+
 interface ExpensesInt {
   provider: string;
   label: string;
   value: number;
+  logo: string;
+  bills: Array<{
+    expire: string;
+    reference: string;
+    value: number;
+  }>
+}
+interface ImportProps {
+  default: string;
+  esModule: boolean;
 }
 
 export default function Expenses() {
-  const [expenses, setExpenses] = useState<Array<ExpensesInt>>([
-    {
-      provider: 'Enel',
-      label: 'Luz',
-      value: 3287.99,
-    },
-    {
-      provider: 'Sabesp',
-      label: 'Àgua',
-      value: 560.68,
-    },
-    {
-      provider: 'Outros',
-      label: 'Diversos',
-      value: 5581.45,
-    },
-  ]);
+  const [billsVisibility, setBillsVisibility] = useState<Array<string>>([]);
+
+  function toggleSetBillsVisibility(id: string, a:string) {
+    if (billsVisibility.includes(id)) {
+      setBillsVisibility(billsVisibility.filter(sid => sid !== id))
+    }
+    else {
+      let newOpen = [...billsVisibility]
+      newOpen.push(id)
+      setBillsVisibility(newOpen)
+    }
+  }
 
   return (
     <main>
       <Amount route="/redespe" />
       <ul className="expenses-list">
-        {expenses.map((expense, index) => {
+        {data.map((expense: ExpensesInt, index: number) => {
           return (
             <li key={'despesa-' + index} className="expense-item">
-              <div className="expense-provider">
-                <h2>{expense.provider}</h2>
-                <p>{expense.label}</p>
+              <div className="expense-details">
+                <div className="expense-provider">
+                  <img src={require(`../../assets/icons/${expense.logo}.svg`)} alt={expense.provider}/>
+                  <div>
+                    <h2>{expense.provider}</h2>
+                    <p>{expense.label}</p>
+                  </div>
+                </div>
+                <p className="expense-value">R${(Math.round(expense.value * 100) / 100).toLocaleString()}</p>
               </div>
-              <p className="expense-value">R${(Math.round(expense.value * 100) / 100).toLocaleString()}</p>
+
+              <div className="expense-buttons">
+                <button className="btn remove-btn" type="button">Remover</button>
+                <button className="btn edit-btn" type="button">Editar</button>
+              </div>
+
+              <span className="more-details" onClick={() => toggleSetBillsVisibility(expense.provider, expense.logo)}>
+                <p>Ver mais detalhes</p>
+                <Arrow />
+              </span>
+
+              <AnimateHeight
+                duration={ 300 }
+                height={ billsVisibility.includes(expense.provider) ? "auto" : 0 }
+              >
+                <ul className={`bills ${billsVisibility ? "show" : ""}`}>
+                  {expense.bills.map(bill => {
+                    return (
+                    <li className="bill">
+                      R${bill.value}
+                    </li>
+                    )
+                  })}
+                </ul>
+              </AnimateHeight>
             </li>
           )
         })}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Total from './Pages/Total/';
-import Expenses from './Pages/Expenses/';
+import Total from './pages/Total/';
+import Expenses from './pages/Expenses/';
 
 function Routes() {
   return (
