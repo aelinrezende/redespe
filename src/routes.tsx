4303297c233d46ab152e4436c3caa2d2929a7cc7
Marco Rezende <marcorezende@outlook.com.br>
2020-10-26 20:55:13 -0300

    adding navbar

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Total from './pages/Total/';
 import Expenses from './pages/Expenses/';
+import Navbar from './components/Navbar/';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Route path="/redespe" exact component={Total}/>
       <Route path="/redespe/expenses" component={Expenses}/>
+      <Route path="/redespe/" component={Navbar}/>
     </BrowserRouter>
   )
 }
