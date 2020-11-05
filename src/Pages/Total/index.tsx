997939e01099572e5784c08226370b6b40c46795
Marco Rezende <marcorezende@outlook.com.br>
2020-11-05 20:02:47 -0300

    adding function to toggle accounts in Total

--- src/Pages/Total/index.tsx
+++ src/Pages/Total/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 
 import Amount from '../../components/Amount/';
 import ExpensesContext, { Accounts, ExpensesInt } from '../../expensesContext';
+import CurrentAccountContext from '../../currentAccountContext';
 
 import { ReactComponent as ToggleIcon } from '../../assets/icons/arrow.svg';
 
@@ -9,7 +10,12 @@ import './styles.scss';
 
 export default function Total() {
   const staticData = useContext(ExpensesContext)[0] as Array<Accounts>;
-  const [currentAccount, setCurrentAccount] = useState<string>('casa_01');
+
+  const currentAccount = useContext(CurrentAccountContext)[0] as string;
+  const setCurrentAccount = useContext(CurrentAccountContext)[1] as Function;
+
+  const [accountsIDs, setAccountsIDs] = useState<Array<string>>([]);
+
   const [expensesTotal, setExpensesTotal] = useState(0);
 
   useEffect(() => {
@@ -22,11 +28,29 @@ export default function Total() {
       }
     }
 
+    if (!accountsIDs.length) {
+      const array: Array<string> = [];
+      staticData.map(account => {
+        array.push(account.id);
+      });
+
+      setAccountsIDs([...array]);
+      console.log(accountsIDs, currentAccount);
+    }
+
     setExpensesTotal(total);
   }, [currentAccount]);
 
   function toggleAccount() {
-    setCurrentAccount('casa_02');
+    const currentAccountIndex = accountsIDs.indexOf(currentAccount);
+
+    if (currentAccountIndex + 1 === accountsIDs.length || accountsIDs.length === 1) {
+      setCurrentAccount(accountsIDs[0]);
+    } else {
+      setCurrentAccount(accountsIDs[currentAccountIndex + 1]);
+    }
+
+    console.log(accountsIDs);
   }
 
   return (
